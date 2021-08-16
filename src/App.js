e3a005c8573cb3f7fa87afd35990627c41b2ef9d
Rajpal Singh <rajpal426a@gmail.com>
2021-08-16 19:02:53 +0530

    Create Function Component and pass Properties & Children through it.
    File changes:
    modified:   src/App.js
    new file:   src/Person/Person.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,15 @@
 import './App.css';
 import React from 'react';
+import Person from './Person/Person';
 
 function App() {  
   return (
-    // Using JSX
     <div className="App">
-      <h1>This is Heading 1</h1>
-      <h2>And It is Heading 2</h2>
+      <h1>Person Details</h1>
+      <Person name="Max" age="20"/>
+      <Person name="John" age="29">He is a Good person</Person>
+      <Person name="Cia" age="19"/>
     </div>
-   
-    /*
-    # Using JS
-    React.createElement('div', {'className': 'App'},
-      React.createElement('h1', null, 'This is Heading 1'),
-      React.createElement('h2', null, 'And It is Heading 2')
-    )
-     */
-
-    /*
-    # It also works (return without brackets)
-    return <article>
-      <h1>Heading</h1>
-      <p>Body</p>
-    </article>
-    */
-
-    /*
-    # It Doesn't works (return starts from next line)
-    return
-    <article>
-      <h1>Heading</h1>
-      <p>Body</p>
-    </article>
-    */  
   );
 }
 export default App;
-
-/* 
-Note:
-1. Always return Single Element (Root Element)
-
-2. Here we returns JSX code which looks similar to HTML tags
-   But it is not HTML, It is JS only. These are defined in JSX.
-   i.e. "class" attribute is written as "className".
-
-3. When we JSX, we always need to import "React" package.
-   Because JSX code is converted in JS using "React.createElement()" method.
-
-4. const element = <h1>Hello, world!</h1>;
-   const element = <h1>Hello, {data.username}</h1>;
-    This funny tag syntax is neither a string nor HTML.
-    It is called JSX, and it is a syntax extension to JavaScript.
-    "data.username", Here "data" is object which contains "username" propery.
-*/
