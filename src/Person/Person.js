fb320bd73bea17bae131c860f35de103019a1d30
Rajpal Singh <rajpal426a@gmail.com>
2021-08-19 22:13:49 +0530

    Styling using ["css module"] (It is scoped styling).
    File changes:
    modified:   src/Person/Person.js
    renamed:    src/Person/Person.css -> src/Person/Person.module.css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,24 +1,81 @@
 import React from "react";
-import './Person.css';
+import classes from './Person.module.css';
 
 const person = (props) => {
     let children;
     if(props.children) children = <p>{props.children}</p>;
 
-    let classes = [];
+    let assignedClasses = [];
     if(props.name.length > 3){
-        classes.push('red');
+        assignedClasses.push('red');
     }
     if(props.name.length > 5){
-        classes.push('bold');
+        assignedClasses.push('bold');
     }
 
     return (
-        <div className="Person">
-            <p className={ classes.join(' ') }>I am {props.name} and i am {props.age}</p>
+        <div className={classes.Person}>
+            <p className={ assignedClasses.join(' ') }>I am {props.name} and i am {props.age}</p>
             <p><input type="text" onChange={props.change} value={props.name}/></p>
             {children}
         </div>
     );
 }
 export default person;
+
+/* Note:
+1. Rename css/scss/sass files as:
+    - style.module.css
+    - style.module.scss
+    - style.module.sass
+
+2. For react-scripts before "react-scripts@2.0.0"
+    Tweak the WebPack config
+    i.   npm run eject
+            confirm it yes
+    ii.  Edit "webpack.config.dev.js"
+            find  "test: /\.css$/,"  (It is inside "module" property)
+            UPDATE from
+            {
+                loader: require.resolve('css-loader'),
+                options: {
+                    importLoaders: 1
+                }
+            }
+            TO
+            {
+                loader: require.resolve('css-loader'),
+                options: {
+                    importLoaders: 1,
+                    modules: true,
+                    localIdentName: '[name]__[local]__[hash:base64:5]'
+                }
+            }
+    
+    iii. Edit "webpack.config.prod.js"
+            find  "test: /\.css$/,"  (It is inside "module" property)
+            UPDATE from
+            {
+                loader: require.resolve('css-loader'),
+                options: {
+                    importLoaders: 1,
+                    minimize: true,
+                    sourceMap: shouldUseSourceMap
+                }
+            }
+            TO
+            {
+                loader: require.resolve('css-loader'),
+                options: {
+                    importLoaders: 1,
+                    modules: true,
+                    localIdentName: '[name]__[local]__[hash:base64:5]'
+                    minimize: true,
+                    sourceMap: shouldUseSourceMap
+                }
+            }
+
+    iv.  Restart the Server
+        -> CTRL + C and confirm
+        -> npm start
+*/
