39fe14452607e52dcad847e6d43bda8c5e3f99fb
Rajpal Singh <rajpal426a@gmail.com>
2021-08-20 09:30:59 +0530

    Radium package Usage. (Pseudo selectors, media query) is used.
    File changes:
    modified:   package-lock.json
    modified:   package.json
    deleted:    src/App.css
    modified:   src/App.js
    modified:   src/Person/Person.js
    deleted:    src/Person/Person.module.css
    deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,59 +1,41 @@
-import './App.css';
-import React, {useState} from 'react';
+import React from 'react';
 import Person from './Person/Person';
+import Radium, {StyleRoot} from 'radium';
 
 const App = () => {
-  const [personState, setPersonState] = useState({
-    persons: [
-      {name: "Max", age: "20"},
-      {name: "John", age: "29"},
-      {name: "Brainly", age: "19"}
-    ]
-  });
-  const [otherState, setOtherState] = useState({
-    otherStateText: "This is the other state"
-  });
-  
-  const onChangeHandler = (index, event) => {
-    let newPersonState = {...personState};
-    newPersonState.persons[index].name = event.target.value;
-    setPersonState(newPersonState);
-  }
-
-  let headingStyle = {
-    color: "white",
-    textShadow: "1px 1px 2px black",
+  const headingStyle = {
+    textAlign: "center",
     border: "1px solid gray",
-    borderRadius: "20px",
-    display: "inline-block",
-    margin: "5px auto",
-    padding: "20px 40px",
-    backgroundColor: "#dddddd"
-  };
-
-  personState.persons.forEach(person => {
-    if(person.name.length > 6){
-      headingStyle.color = 'green';
-      headingStyle.textShadow = '1px 1px 2px white';
+    maxWidth: "400px",
+    margin: "20px auto",
+    padding: "20px",
+    cursor: "pointer",
+    ":hover": {
+      color: "white",
+      backgroundColor: "black"
     }
-  });
-
+  };
   return (
-    <div className="App">
-      <h1 style={headingStyle}>Person Details</h1>
-      <Person
-        change={onChangeHandler.bind(this, 0)}
-        name={personState.persons[0].name}
-        age={personState.persons[0].age}/>
-      <Person
-        change={onChangeHandler.bind(this, 1)}
-        name={personState.persons[1].name}
-        age={personState.persons[1].age}>He is a Good person</Person>
-      <Person
-        change={onChangeHandler.bind(this, 2)}
-        name={personState.persons[2].name}
-        age={personState.persons[2].age}/>
-    </div>
+    <StyleRoot>
+      <div>
+        <h1 style={headingStyle}>List of All Persons</h1>
+        <Person name="Max"/>
+        <Person name="Rock"/>
+        <Person name="Ryan"/>
+      </div>
+    </StyleRoot>
   );
 }
-export default App;
+export default Radium(App);
+
+/* Note:
+1. If you want to use [media query, keyframes] in any of the component, then you must
+    wrap your top-level App component with <StyleRoot>.
+2. media queries and keyframes may be used in any of the child component,
+    But you wrap only Root component of your App.
+3. "export default Radium(<component-name>)" is Always must.
+    <component-name> is the component in which radium styling is used.
+
+
+    [Media query is used Person Component.]
+*/
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,81 +1,23 @@
 import React from "react";
-import classes from './Person.module.css';
+import Radium from "radium";
 
 const person = (props) => {
-    let children;
-    if(props.children) children = <p>{props.children}</p>;
-
-    let assignedClasses = [];
-    if(props.name.length > 3){
-        assignedClasses.push('red');
-    }
-    if(props.name.length > 5){
-        assignedClasses.push('bold');
-    }
-
+    const paraStyle = {
+        textAlign: "center",
+        backgroundColor: "orange",
+        "@media (max-width: 800px)": {
+            color: "yellow",
+            backgroundColor: "red"
+        },
+        "@media (max-width: 600px)": {
+            color: "white",
+            backgroundColor: "green"
+        }
+    };
     return (
-        <div className={classes.Person}>
-            <p className={ assignedClasses.join(' ') }>I am {props.name} and i am {props.age}</p>
-            <p><input type="text" onChange={props.change} value={props.name}/></p>
-            {children}
+        <div>
+            <p style={paraStyle}>I am {props.name}</p>           
         </div>
     );
 }
-export default person;
-
-/* Note:
-1. Rename css/scss/sass files as:
-    - style.module.css
-    - style.module.scss
-    - style.module.sass
-
-2. For react-scripts before "react-scripts@2.0.0"
-    Tweak the WebPack config
-    i.   npm run eject
-            confirm it yes
-    ii.  Edit "webpack.config.dev.js"
-            find  "test: /\.css$/,"  (It is inside "module" property)
-            UPDATE from
-            {
-                loader: require.resolve('css-loader'),
-                options: {
-                    importLoaders: 1
-                }
-            }
-            TO
-            {
-                loader: require.resolve('css-loader'),
-                options: {
-                    importLoaders: 1,
-                    modules: true,
-                    localIdentName: '[name]__[local]__[hash:base64:5]'
-                }
-            }
-    
-    iii. Edit "webpack.config.prod.js"
-            find  "test: /\.css$/,"  (It is inside "module" property)
-            UPDATE from
-            {
-                loader: require.resolve('css-loader'),
-                options: {
-                    importLoaders: 1,
-                    minimize: true,
-                    sourceMap: shouldUseSourceMap
-                }
-            }
-            TO
-            {
-                loader: require.resolve('css-loader'),
-                options: {
-                    importLoaders: 1,
-                    modules: true,
-                    localIdentName: '[name]__[local]__[hash:base64:5]'
-                    minimize: true,
-                    sourceMap: shouldUseSourceMap
-                }
-            }
-
-    iv.  Restart the Server
-        -> CTRL + C and confirm
-        -> npm start
-*/
+export default Radium(person);
